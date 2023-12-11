666dda44b8123061b5f1fc65847b0965aacdb9dc
ab-ps-2023 <153479325+ab-ps-2023@users.noreply.github.com>
2023-12-10 22:38:26 -0600

    Update dataset.py

--- transfomer_model/dataset.py
+++ transfomer_model/dataset.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
 
-class BilingualDataset(Dataset):
+class TextToSQLDataset(Dataset):
 
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len):
         super().__init__()
